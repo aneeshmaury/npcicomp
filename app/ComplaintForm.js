7d78e6f2fa192904fdfd1d679343fc9da1e98a39
aneeshmaury <aneeshmaury.in@gmail.com>
2025-04-29 12:25:35 +0530

    dfgdfgdffdg

--- app/ComplaintForm.js
+++ app/ComplaintForm.js
@@ -2,6 +2,8 @@
 
 import React, { useState } from 'react';
 import { FaMinus } from "react-icons/fa";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+
 
 function ComplaintForm() {
   const [formData, setFormData] = useState({
@@ -50,57 +52,78 @@ function ComplaintForm() {
           <div className="w-full md:w-1/2">
             <label className="block text-sm tracking-wider
   font- text-purple-900 ">Nature of transaction</label>
-            <select
+            <div
               name="natureOfTransaction"
               value={formData.natureOfTransaction}
               onChange={handleChange}
               className="w-full  border-b border-gray-300 "
             >
-              <option>--Select Nature of transaction--</option>
-              {/* Add your options here */}
-            </select>
+              <div className=' flex justify-between items-center'>
+               <div className='text-md'>Person to Person</div>
+            <div className='text-xl'><MdOutlineKeyboardArrowDown /></div> 
+              </div>
+              
+            </div>
             <small className="text-xs italic text-gray-500">Please select the Nature of Transaction from drop down list</small>
           </div>
 
           <div className="w-full md:w-1/2">
-            <label className="block text-sm tracking-wider
+            <label className="block mt-[-19] text-sm tracking-wider
   font- text-purple-900">Issue</label>
-            <select
+            <div
               name="issueType"
               value={formData.issueType}
               onChange={handleChange}
               className="w-full  border-b border-blue-500 "
             >
-              <option>--Select Issue Type--</option>
-              {/* Add your options here */}
-            </select>
+               <div className=' flex justify-between items-center'>
+               <div className='text-md'>Fraudulent transaction</div>
+            <div className='text-xl'><MdOutlineKeyboardArrowDown /></div> 
+              </div>
+            </div>
             <small className="text-xs italic text-gray-500">Please select the issue from drop down list</small>
           </div>
         </div>
 
         <div className="w-full">
-          <label className="block text-sm tracking-wider
+          <label className="block mt-[-19] text-sm tracking-wider
   font- text-purple-900">Comments</label>
           <textarea
             name="comments"
-            rows="2"
+            rows="1"
             maxLength="500"
-            placeholder="Provide brief about your issue. Not greater than 500 characters"
+            placeholder=""
             value={formData.comments}
             onChange={handleChange}
             className="w-full  border-b border-gray-300"
           ></textarea>
+                      <small className="text-xs italic text-gray-500">Provide brief about your issue. Not greater than 500 characters</small>
         </div>
 
         <div className="w-full">
-          <label className="block text-sm tracking-wider
+          <label className="block mt-[-19] text-sm tracking-wider
+  font- text-purple-900 ">Transaction ID</label>
+          <input
+            type="text"
+            name="transactionId"
+            placeholder=""
+            value={formData.transactionId}
+            onChange={handleChange}
+            className="w-full  border-b border-gray-300"
+          />
+                      <small className="text-xs italic text-gray-500">Please enter your 12-digit transaction reference number. E.g. 500112345678</small>
+        </div>
+
+
+        <div className="w-full">
+          <label className="block mt-[-19] text-sm tracking-wider
   font- text-purple-900">Bank Name</label>
           <select
             name="bankName"
             required
             value={formData.bankName}
             onChange={handleChange}
-            className="w-full  border-b border-gray-300 "
+            className="w-full ml-[-3]  border-b border-gray-300 "
           >
             <option>--Select Bank Name--</option>
             <option>State Bank of India</option>
@@ -110,22 +133,11 @@ function ComplaintForm() {
           <small className="text-xs italic text-gray-500">Please select your bank from drop down list</small>
         </div>
 
-        <div className="w-full">
-          <label className="block text-sm tracking-wider
-  font- text-purple-900 ">Transaction ID</label>
-          <input
-            type="text"
-            name="transactionId"
-            placeholder="Please enter your 12-digit transaction reference number. E.g. 500112345678"
-            value={formData.transactionId}
-            onChange={handleChange}
-            className="w-full  border-b border-gray-300"
-          />
-        </div>
+     
 
         <div className="flex gap-6">
           <div className="w-full md:w-1/2">
-            <label className="block text-sm tracking-wider
+            <label className="block mt-[-19] text-sm tracking-wider
   font- text-purple-900">Rupees</label>
             <input
               type="number"
@@ -138,7 +150,7 @@ function ComplaintForm() {
           </div>
 
           <div className="w-full md:w-1/2">
-            <label className="block text-sm tracking-wider
+            <label className="block mt-[-19] text-sm tracking-wider
   font- text-purple-900">Paisa</label>
             <input
               type="number"
@@ -152,7 +164,7 @@ function ComplaintForm() {
         </div>
 
         <div className="w-full">
-          <label className="block text-sm tracking-wider
+          <label className="block mt-[-19] text-sm tracking-wider
   font- text-purple-900">Date of Transaction</label>
           <input
             type="date"
@@ -165,7 +177,7 @@ function ComplaintForm() {
         </div>
 
         <div className="w-full">
-          <label className="block text-sm tracking-wider
+          <label className="block mt-[-19] text-sm tracking-wider
   font- text-purple-900 ">Email ID</label>
           <input
             type="email"
